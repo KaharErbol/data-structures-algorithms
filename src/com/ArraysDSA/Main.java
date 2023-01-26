3fe1b722c5aef8109bdff21eedf3de11f3b21726
Kahar Erbol <kaharerbol@gmail.com>
2023-01-26 11:21:50 -0800

    Added the whole java project file -- LinkedList

---
+++ src/com/ArraysDSA/Main.java
@@ -0,0 +1,31 @@
+package com.ArraysDSA;
+
+import java.util.Arrays;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // write your code here
+//        var list = new LinkedList();
+//
+//        list.addLast(10);
+//        list.addLast(20);
+//        list.addLast(30);
+//        list.addLast(40);
+//        list.addLast(50);
+//        list.addLast(60);
+//
+//        System.out.println(list.getKthFromTheEnd(2));
+//        list.printMiddle();
+//
+//        var revenue = 175 * 60 + 185 * 60 + 250 * 30;
+//        System.out.println(revenue);
+
+        var list1 = new LinkedList();
+        var list2 = new LinkedList();
+
+        list1.addLast(6);
+
+
+    }
+}
